23ef0bc56dc4de0f124ecd8dce885369ff7d3f3f
Mike Pangas <mpangas@ufl.edu>
2022-10-17 16:41:49 -0400

    Lesson 11

--- week-2-javascript/game1/js/main.js
+++ week-2-javascript/game1/js/main.js
@@ -1,23 +1,34 @@
-// Code Challenge 1
+// First interactive game
 
-// Return random letter from your name
-const myName = "Michael";
-const randomIndex = Math.floor(Math.random() * myName.length);
-console.log(myName.charAt(randomIndex));
-
-// Ternary Operators
-
-let playerOne = "scissors"
-let computer = "rock";
-let result =
-    playerOne === computer ? "Tie game!"
-        : playerOne === "rock" && computer === "paper"
-            ? "Computer wins!"
-            : playerOne === "paper" && computer === "scissors"
-                ? "Computer wins!"
-                : playerOne === "scissors" && computer === "rock"
-                    ? "Computer wins!"
-                    : "playerOne wins!";
-console.log(result);
-
-
+let playGame = confirm("Shall we play rock, paper, scissors?");
+if (playGame) {
+    let playerChoice = prompt("Please enter rock, paper, or scissors.");
+    if (playerChoice) {
+        let playerOne = playerChoice.trim().toLowerCase();
+        if (playerOne === "rock" || playerOne === "paper" || playerOne === "scissors") {
+            let computerChoice = Math.floor(Math.random() * 3 + 1);
+            let computer = computerChoice === 1 ? "rock"
+                : computerChoice === 2 ? "paper"
+                    : "scissors";
+            let result =
+                playerOne === computer
+                    ? "Tie game!"
+                    : playerOne === "rock" && computer === "paper"
+                        ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                        : playerOne === "paper" && computer === "scissors"
+                            ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                            : playerOne === "scissors" && computer === "rock"
+                                ? `playerOne: ${playerOne}\nComputer: ${computer}\nComputer wins!`
+                                : `playerOne: ${playerOne}\nComputer: ${computer}\nplayerOne wins!`;
+            alert(result);
+            let playAgain = confirm("Play Again?");
+            playAgain ? location.reload() : alert("Ok, thanks for playing.");
+        } else {
+            alert("You didn't enter rock, paper, or scissors.");
+        }
+    } else {
+        alert("I guess you changed your mind. Maybe next time.");
+    }
+} else {
+    alert("Ok, maybe next time.");
+}
