60d99c8201c7129a38400b36346ee3cd7f783512
Mike Pangas <mpangas@ufl.edu>
2022-10-17 15:03:09 -0400

    Lessons 3, 4, 5, 6

--- week-2-javascript/js/main.js
+++ week-2-javascript/js/main.js
@@ -1,1 +1,6 @@
-console.log("Hello world");
+// Code Challenge 1
+
+// Return random letter from your name
+const myName = "Michael"
+const randomIndex = Math.floor(Math.random()*myName.length);
+console.log(myName.charAt(randomIndex));
