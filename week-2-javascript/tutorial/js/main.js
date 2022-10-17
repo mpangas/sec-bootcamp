c4718ad43edcc2702d621b1cc6e9423aef9bae35
Mike Pangas <mpangas@ufl.edu>
2022-10-17 17:06:27 -0400

    Lesson 12

--- week-2-javascript/tutorial/js/main.js
+++ week-2-javascript/tutorial/js/main.js
@@ -20,4 +20,19 @@ let result =
                     : "playerOne wins!";
 console.log(result);
 
+// Loops
 
+let name = "Dave";
+let counter = 0;
+let myLetter;
+while(counter <= 3) {
+    myLetter = name[counter];
+    console.log(myLetter);
+    if(counter === 1) {
+        counter += 2;
+        continue;
+    }
+    if(myLetter === "v") break;
+    counter++;
+}
+console.log(counter);
