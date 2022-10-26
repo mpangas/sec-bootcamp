c0eb82cd7248a94c134d6d3a9e0ae45941c0b7a8
Mike Pangas <mpangas@ufl.edu>
2022-10-26 16:05:00 -0400

    Lesson 25

--- week-2-javascript/modsandfuncs/js/main.js
+++ week-2-javascript/modsandfuncs/js/main.js
@@ -9,3 +9,31 @@ console.log(me);
 console.log(Guitars.default());
 console.log(Guitars.shredding());
 console.log(Guitars.plucking());
+
+// Higher Order Functions
+
+// Function that does at least one of the following
+// - Takes one or more functions as an argument
+// - Returns a function as the result
+
+import { posts } from "./posts.js";
+
+posts.forEach(post => {
+    console.log(post);
+})
+console.clear();
+
+const filteredPosts = posts.filter((post) => {
+    return post.userId === 5;
+});
+console.log(filteredPosts);
+
+const mappedPosts = filteredPosts.map(post => {
+    return post.id * 10;
+});
+console.log(mappedPosts);
+
+const reducedPosts = mappedPosts.reduce((sum, post) => {
+    return sum + post;
+});
+console.log(reducedPosts);
