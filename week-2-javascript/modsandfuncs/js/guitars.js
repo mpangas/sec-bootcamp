d69666ff0a0a80a486546753ac81203ae12d3b95
Mike Pangas <mpangas@ufl.edu>
2022-10-25 22:14:20 -0400

    Lesson 24 (Part 2)

---
+++ week-2-javascript/modsandfuncs/js/guitars.js
@@ -0,0 +1,16 @@
+// Modules
+
+export default function playGuitar () {
+    return "Playing guitar!";
+}
+
+export const shredding = () => {
+    return "Shredding!";
+}
+
+export const plucking = () => {
+    return "Plucking the strings...";
+}
+
+// export default playGuitar;
+// export {shredding, plucking};
--- week-2-javascript/modsandfuncs/js/main.js
+++ week-2-javascript/modsandfuncs/js/main.js
@@ -1,2 +1,11 @@
 // Modules
+// import playGuitar from "./guitars.js";
+// import {shredding as shred, plucking as pluck} from "./guitars.js";
+import * as Guitars from "./guitars.js"
+import User from "./user.js";
+const me = new User("email@email.com", "Dave");
+console.log(me);
 
+console.log(Guitars.default());
+console.log(Guitars.shredding());
+console.log(Guitars.plucking());
