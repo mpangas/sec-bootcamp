1e46234e50bbc989377e6bef81328badbf762f4b
Mike Pangas <mpangas@ufl.edu>
2022-11-12 22:55:47 -0500

    Lesson 11

---
+++ week-3-nodejs/lesson11/controllers/logoutController.js
@@ -0,0 +1,32 @@
+const usersDB = {
+    users: require('../model/users.json'),
+    setUsers: function (data) { this.users = data }
+}
+
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogout = async (req, res) => {
+    // On client, also delete the accessToken
+    const cookies = req.cookies;
+    if (!cookies?.jwt) return res.sendStatus(401); //No content
+    const refreshToken = cookies.jwt;
+    // Is refreshToken in db?
+    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    if (!foundUser) {
+        res.clearCookie('jwt', { httpOnly: true });
+        return res.sendStatus(204);
+    }
+    // Delete refreshToken in db
+    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
+    const currentUser = {...foundUser, refreshToken: ''};
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+        path.join(__dirname, '..', 'model', 'users.json'),
+        JSON.stringify(usersDB.users)
+    );
+    res.clearCookie('jwt', { httpOnly:true }); // secure: true - only serves on https
+    res.sendStatus(204);
+}
+
+module.exports = { handleLogout };
