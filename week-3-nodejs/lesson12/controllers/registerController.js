8e02f10c25c6c2aed216be7a28d22b87516299d4
Mike Pangas <mpangas@ufl.edu>
2022-11-23 15:22:44 -0500

    Lesson 14

--- week-3-nodejs/lesson12/controllers/registerController.js
+++ week-3-nodejs/lesson12/controllers/registerController.js
@@ -1,32 +1,22 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
     // check for duplicate usernames in the db
-    const duplicate = usersDB.users.find(person => person.username === user);
+    const duplicate = await User.findOne({ username: user }).exec();
     if (duplicate) return res.sendStatus(409); //Conflict 
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
-        //store the new user
-        const newUser = {
+        //create and store the new user
+        const result = await User.create({
             "username": user,
-            "roles": { "User": 2001 },
             "password": hashedPwd
-        };
-        usersDB.setUsers([...usersDB.users, newUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
-        console.log(usersDB.users);
+        }); 
+        console.log(result);
         res.status(201).json({ 'success': `New user ${user} created!` });
     } catch (err) {
         res.status(500).json({ 'message': err.message });
