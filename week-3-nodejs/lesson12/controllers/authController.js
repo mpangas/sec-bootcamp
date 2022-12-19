3912307bd5ebdafbadb057e1b7331c413dfe2410
Mike Pangas <mpangas@ufl.edu>
2022-12-18 20:25:07 -0500

    Final Lesson

--- week-3-nodejs/lesson12/controllers/authController.js
+++ week-3-nodejs/lesson12/controllers/authController.js
@@ -1,17 +1,12 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
-
 const jwt = require('jsonwebtoken');
-const fsPromises = require('fs').promises;
-const path = require('path');
 
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
-    const foundUser = usersDB.users.find(person => person.username === user);
+
+    const foundUser = await User.findOne({ username: user }).exec();
     if (!foundUser) return res.sendStatus(401); //Unauthorized 
     // evaluate password 
     const match = await bcrypt.compare(pwd, foundUser.password);
@@ -34,13 +29,10 @@ const handleLogin = async (req, res) => {
             { expiresIn: '1d' }
         );
         // Saving refreshToken with current user
-        const otherUsers = usersDB.users.filter(person => person.username !== foundUser.username);
-        const currentUser = { ...foundUser, refreshToken };
-        usersDB.setUsers([...otherUsers, currentUser]);
-        await fsPromises.writeFile(
-            path.join(__dirname, '..', 'model', 'users.json'),
-            JSON.stringify(usersDB.users)
-        );
+        foundUser.refreshToken = refreshToken;
+        const result = await foundUser.save();
+        console.log(result);
+        
         res.cookie('jwt', refreshToken, { httpOnly: true, maxAge: 24 * 60 * 60 * 1000 });
         res.json({ accessToken });
     } else {
--- week-3-nodejs/lesson12/controllers/logoutController.js
+++ week-3-nodejs/lesson12/controllers/logoutController.js
@@ -1,31 +1,25 @@
-const usersDB = {
-    users: require('../model/users.json'),
-    setUsers: function (data) { this.users = data }
-}
-
-const fsPromises = require('fs').promises;
-const path = require('path');
+const User = require('../model/User');
 
 const handleLogout = async (req, res) => {
     // On client, also delete the accessToken
+
     const cookies = req.cookies;
     if (!cookies?.jwt) return res.sendStatus(401); //No content
     const refreshToken = cookies.jwt;
+
     // Is refreshToken in db?
-    const foundUser = usersDB.users.find(person => person.refreshToken === refreshToken);
+    const foundUser = await User.findOne({ refreshToken }).exec();
     if (!foundUser) {
         res.clearCookie('jwt', { httpOnly: true });
         return res.sendStatus(204);
     }
+
     // Delete refreshToken in db
-    const otherUsers = usersDB.users.filter(person => person.refreshToken !== foundUser.refreshToken);
-    const currentUser = {...foundUser, refreshToken: ''};
-    usersDB.setUsers([...otherUsers, currentUser]);
-    await fsPromises.writeFile(
-        path.join(__dirname, '..', 'model', 'users.json'),
-        JSON.stringify(usersDB.users)
-    );
-    res.clearCookie('jwt', { httpOnly:true }); // secure: true - only serves on https
+    foundUser.refreshToken = '';
+    const result = await foundUser.save();
+    console.log(reuslt);
+
+    res.clearCookie('jwt', { httpOnly: true, sameSite: 'None', secure: true});
     res.sendStatus(204);
 }
 
