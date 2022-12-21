05dac5eeff9a91e09fbc90a9a7aa628426bea2d9
Mike Pangas <mpangas@ufl.edu>
2022-12-20 20:45:08 -0500

    Lesson 5

--- reactjs/myproj/src/Content.js
+++ reactjs/myproj/src/Content.js
@@ -6,11 +6,17 @@ const Content = () => {
         const int = Math.floor(Math.random() * 3);
         return names[int];
     }
+    
+    const handleClick = (name) => {
+        console.log(`${name} clicked the button`);
+    }
+    
     return (
         <main>
             <p>
                 Hello {handleNameChange()}
             </p>
+            <button onDoubleClick={() => handleClick('Dave')}>Click It</button>
         </main>
     )
 }
